b61a32120b44d178caf3fd631b1418e994b086eb
braybergtest <44968028+braybergtest@users.noreply.github.com>
2018-12-07 05:52:28 -0600

    Add files via upload

---
+++ assignment8.java
@@ -0,0 +1,82 @@
+//Brayden Berglund
+//Assignment 8
+//program does not print anything, is not fully implemented
+
+import java.util.List;
+import java.util.ArrayList;
+
+//used to update board after each turn
+public List<board> boardMoves() {
+    List<board> newBoard = new ArrayList<board>();
+    int i, j;
+    //setting up the board
+    for(i=0; i < 5; i++) {
+        for(j=0; j <=i; j++) {
+            //not implemented
+        }
+
+    }
+    return
+}
+
+//used to set up the board
+public class playBoard {
+    //setting up the list and it's objects
+    List<playBoard> pegs = newArrayList<playBoard>();
+
+    board bgame;
+    //constructor
+    public playBoard(board bgame) {
+        this.bgame = bgame;
+    }
+    public printBoard() {
+
+    }
+    //Need to call list board
+
+
+}
+//class that will deal with the moves
+public class Move {
+    pair from;
+    pair over;
+    pair to;
+
+    public Move(pair from, pair over, pair to) {
+        this.from = from;
+        this.over = over;
+        this.to = to;
+    }
+
+    public possibleMoves() {
+        /*
+        moves = (
+            (0,1,3),
+            (0,2,5),
+            (1,3,6),
+              (1,4,8),
+              (2,4,7),
+              (2,5,9),
+              (3,6,10),
+              (3,7,12),
+              (4,7,11),
+              (4,8,13),
+              (5,8,12),
+              (5,9,14),
+              (3,4,5),
+              (6,7,8),
+              (7,8,9),
+              (10,11,12),
+              (11,12,13),
+              (12,13,14)
+            )
+        */
+    }
+
+
+}
+
+
+public static void main(String [] args) {
+
+}
